8f0a0efd804d24b3490c06da335d2e6cedd2e1c6
ultimasora <alextien.tran@yahoo.com>
2017-03-12 15:11:44 -0700

    move fixes

--- displayables.js
+++ displayables.js
@@ -338,6 +338,7 @@ Declare_Any_Class( "Game_Scene",  // Displayable object that our class Canvas_Ma
                 this.blockman.addState("rotating", [], false);
                 this.blockman.addState("rotated", [[0,1,2,3,4,5,6,7,8,9,10,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]]);                    
                 this.firstFrame = false;
+                console.log( this.blockman_loc );
                 this.blockman.moveTo(this.blockman_loc);
                 if (this.blockman.moves.length)
                     console.log(this.blockman.moves);
