39380906b0bb21affe1828c0edffe1d4b8d172ea
ultimasora <alextien.tran@yahoo.com>
2017-03-12 14:31:06 -0700

    blockman moveTo has actual mouse coord input now

--- displayables.js
+++ displayables.js
@@ -116,7 +116,7 @@ var global_picker;  // experimental
 Declare_Any_Class( "Game_Scene",  // Displayable object that our class Canvas_Manager can manage.  This one draws the scene's 3D shapes.
   { 'construct': function( context )
       { this.shared_scratchpad = context.shared_scratchpad;
-        this.define_data_members( { picker: new Picker( canvas ), assignedPickColors: false, objIndex: 0, moved: false, pausable_time: 0, firstFrame: true, blockman: new Blockman(9, "original"), cubeman_transform: mat4() } );
+        this.define_data_members( { picker: new Picker( canvas ), assignedPickColors: false, objIndex: 0, moved: false, pausable_time: 0, firstFrame: true, blockman: new Blockman(9, "original"), cubeman_transform: mat4(), blockman_loc : 1 } );
 		
 		global_picker = this.picker;	// experimental
 		
@@ -166,7 +166,7 @@ Declare_Any_Class( "Game_Scene",  // Displayable object that our class Canvas_Ma
 			gl.readPixels( e.clientX, e.clientY, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, readout );
 			gl.bindFramebuffer( gl.FRAMEBUFFER, null );
 			// console.log( readout );
-      console.log( global_picker.find( [ e.clientX, e.clientY ] ) ); } } ) (this), false );
+      console.log( this.blockman_loc = global_picker.find( [ e.clientX, e.clientY ] ) ); } } ) (this), false );
       },
     'update_strings': function( user_interface_string_manager )       // Strings that this displayable object (Animation) contributes to the UI:
       {
@@ -342,7 +342,7 @@ Declare_Any_Class( "Game_Scene",  // Displayable object that our class Canvas_Ma
                 this.blockman.addState("rotating", [], false);
                 this.blockman.addState("rotated", [[0,1,2,3,4,5,6,7,8,9,10,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]]);                    
                 this.firstFrame = false;
-                this.blockman.moveTo(1);
+                this.blockman.moveTo(this.blockman_loc);
                 if (this.blockman.moves.length)
                     console.log(this.blockman.moves);
             }                
